cfe2df443d713ac8cb995bdc5acda990648660d2
root <root@puppet.example.com>
2013-04-26 19:45:31 +0800

    add api and client file

---
+++ api.py
@@ -0,0 +1,210 @@
+#! /usr/bin/env python2.7
+
+__version__ = '1.0.0'
+__author__='paperplane'
+
+'''
+Python Client SDK For Zabbix API
+'''
+
+
+import json 
+import urllib2
+import logging
+
+urllib2.socket.setdefaulttimeout(10)
+
+#============LOG SECTION==============#
+class Singleton(object):
+    def __new__(cls,*args,**kwargs):
+        if '_inst' not in vars(cls):
+            cls._inst = super(Singleton,cls).__new__(cls,*args,**kwargs)
+        return cls._inst
+
+class Logger(Singleton):
+    '''
+    logging is wired, so make sure do that only once.
+    '''
+    _no_handlers = True
+    def __init__(self,logfilepath='/var/log/zabbix.log'):
+        self._setup_logging()
+        if self._no_handlers:
+            self._setup_handlers(logfilepath=logfilepath)
+
+    def _setup_logging(self):
+        self.logger = logging.getLogger('Zabbix API')
+
+    def _setup_handlers(self,logfilepath='/var/log/zabbix.log'):
+        self.logger.setLevel(logging.DEBUG)
+        handler = logging.FileHandler(logfilepath,'a')
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+        self._no_handlers = False
+
+#============CALL SECTION=============#
+def checkparams(func):
+    '''
+    check parameter conflicts.
+    '''
+    def wrapper(client,operation,method,*args,**kw):
+        if args and kw:
+            message = 'parameters error! both list and dict params in the same method'
+            Logger().logger.error(message)
+            raise ZabbixAPIException(message)
+        return func(client,operation,method,*args,**kw)
+    return wrapper
+
+def checkauth(func):
+    def wrapper(client,operation,method,*args,**kw):
+        if not client.auth:
+            message = 'auth error! please login'
+            Logger().logger.error(message)
+            raise ZabbixAPIException(message)
+        return func(client,operation,method,*args,**kw)
+    return wrapper
+
+@checkparams
+@checkauth
+def do_post(client,operation,method,*args,**kw):
+    '''
+    do http_post for zabbix api method.
+    '''
+    headers = {"Content-Type": "application/json"}
+    params = None
+    if args:
+        params = args
+    else:
+        params = kw
+    request = urllib2.Request(client.url,client.json_obj(method+'.'+operation,params),headers)
+    try:
+        result = urllib2.urlopen(request)
+    except urllib2.URLError:
+        message = 'URLError! API calls failed'
+        Logger.logger.error(message)
+        raise ZabbixAPIException()
+    else:
+        result = json.loads(result.read())
+        if 'error' in result:
+            message = result['error']
+            Logger().logger.error(message)
+            raise ZabbixAPIException(message)
+        Logger().logger.info('API calls succeed with resource: %s, action: %s'%(method,operation))
+        return result['result']
+
+#============EXCEPTION SECTION=============#
+class ZabbixAPIException(Exception):
+    '''
+    raise APIError with Error Message.
+    '''
+    def __init__(self,message):
+        self.message = message
+    
+    def __str__(self):
+        return repr(self.message)
+
+
+#=============API OBJECT SECTION=============#
+class APIClient(object):
+    '''
+    API Client For Zabbix.
+    '''
+    def __init__(self,domain,username,password):
+        self.url = domain.rstrip('/')+'/api_jsonrpc.php'
+        self.username = username
+        self.password = password
+        self.auth = ''
+        self.logger = Logger().logger
+        self.object_list =  ['user','host','item','template','action']
+        self.id = 0
+
+    def login(self):
+        '''
+        user login
+        '''
+        user_info = {'user':self.username,
+            'password':self.password
+        }
+        obj = self.json_obj('user.login',user_info)
+
+        response = self.do_request(obj)
+        if 'error' in response:
+            message = response['error']
+            self.logger.error(message)
+            raise ZabbixAPIException(message)
+        self.auth = response['result']
+        self.logger.info('zabbix login with user: %s, version: %s' % (self.username,self.version()))
+
+    def version(self):
+        '''
+        return api version.
+        '''
+        version_obj = self.json_obj('apiinfo.version',[])
+        return self.do_request(version_obj)['result']
+
+    def json_obj(self,method,params):
+        '''
+        request json object in zabbix api.
+        '''
+        obj = {
+            "jsonrpc": "2.0",
+            "method": method,
+            "params": params,
+            "auth":self.auth,
+            "id": self.id
+        }
+        return json.dumps(obj)
+
+    def do_request(self,json_obj):
+        '''
+        use for login and get version.
+        '''
+        headers = {"Content-Type": "application/json"}
+        request = urllib2.Request(self.url,json_obj,headers)
+        try:
+            result = urllib2.urlopen(request)
+        except urllib2.URLError:
+            message = 'URLError error! login fails'
+            self.logger.error(message)
+            raise ZabbixAPIException(message)
+        else:
+            return json.loads(result.read())
+
+    def __getattr__(self,attr):
+        if '__'in attr:
+            return getattr(self.get,attr)
+        return _Callable(self,attr)
+
+
+class _Callable(object):
+    def __init__(self,client,method):
+        self.client = client
+        self.method = method
+    def __getattr__(self,attr):
+        operations = ['get','create']
+        if attr in operations:
+            return _Executable(self.client,attr,self.method)
+        method = '%s.%s'%(self.method,attr)
+        return _Callable(self.client,method)
+    def __str__(self):
+        return '%s'%(self.method)
+
+class _Executable(object):
+    def __init__(self,client,operation,method):
+        self.client = client
+        self.operation = operation
+        self.method = method
+    def __call__(self,*args,**kw):
+        return do_post(self.client,self.operation,self.method,*args,**kw)
+    def __str__(self):
+        return '%s %s'%(self.operation,self.method)
+
+#=================TEST SECTION=================#
+def main():
+    api = APIClient(domain='http://monitor.ifeng.com',username='guestapi',password='guestapi')
+    api.login()
+    #dicts = {"output": "extend","filter": {"host": ["Zabbix server","Linux server"]}}
+    print api.host.get(filter={"host": ["Zabbix server","Linux server"]})
+
+if __name__ == '__main__':
+    main()
---
+++ client.py
@@ -0,0 +1,90 @@
+#! /usr/bin/env python2.7
+
+'''
+simple cli tools for zabbix api
+it is in development
+more function to be added
+better realization of the method remains to be found
+'''
+
+import argparse
+import api
+import sys
+from getpass import getpass
+
+def auth_opt():
+    parser = argparse.ArgumentParser(add_help=False)
+
+    group = parser.add_argument_group('authentication')
+    group.add_argument('-s','--server',action='store',default='http://monitor.ifeng.com',help='zabbix server url')
+    group.add_argument('-u','--user',action='store',default='guestapi',help='zabbix user name')
+    group.add_argument('-p','--password',action='store',default='guestapi',help='zabbix user password')
+    return parser
+
+def get_options():
+    '''
+    commad line options
+    '''
+    parser = argparse.ArgumentParser(prog='zabbix',parents=[auth_opt()],)
+
+    subparsers = parser.add_subparsers(help='method support currently',dest='method')
+
+    create_parser = subparsers.add_parser('create',help='Create zabbix object')
+    create_parser.add_argument('object',action='store',choices=('user','host'),help='zabbix object to create')
+    create_parser.add_argument('--passwd',action='store',help='new user password')
+    create_parser.add_argument('--usrgrps',action='store',help='group that user belong to')
+    create_parser.add_argument('--groups',action='store',help='host groups to add the host to')
+    create_parser.add_argument('--interfaces',action='store',help='interfaces to be created for the host')
+
+    delete_parser = subparsers.add_parser('delete', help='Remove zabbix object')
+    delete_parser.add_argument('object', action='store',choices=('user',),help='The zabbix object to remove')
+    delete_parser.add_argument('--userid',action='store',help='id of the user to remove')
+
+    get_parser = subparsers.add_parser('get', help='Get zabbix object information')
+    get_parser.add_argument('object', action='store',choices=('user','host','item'),help='Zabbix object to get')
+    get_parser.add_argument('--output',action='store',help='choose attribute of the object to display')
+    
+    parser.add_argument('--version', action='version',version = '%(prog)s 1.0')
+    return parser
+
+def show_help(parser):
+    parser.print_help()
+    sys.exit(1)
+
+def main():
+    options = get_options().parse_args()
+
+    print options
+    if not options.server:
+        show_help(get_options())
+
+    if not options.user:
+        options.user = raw_input('username:')
+    
+    if not options.password:
+        options.password = getpass()
+
+    zapi = api.APIClient(options.server,options.user,options.password)
+    method = options.method
+    object = options.object
+    zapi.login()
+    try:
+        obj = getattr(zapi,object)
+    except:
+        print >> sys.stderr, 'object not support'
+        sys.exit(1)
+    try:
+        func = getattr(obj,method)
+    except:
+        print >> sys.stderr, 'method not support'
+        sys.exit(1)
+   
+    if method == 'get':
+        print func(output=options.output)
+    elif method == 'delete':
+        print func(userid=options.userid) 
+    else:
+        func(passwd=options.passwd,usrgrps=options.usrgrps,groups=options.groups,interfaces=options.interfaces)
+
+if __name__ == '__main__':
+    main()
